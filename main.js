19907992b605197a247db8ea7ca82a66150e5f6f
leanbenac <benacleandro@gmail.com>
2022-04-04 14:10:03 -0300

    Finished project

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-const app= document.getElementById('typewriter');
+const app = document.getElementById("typewriter");
 
 const typewriter = new Typewriter(app, {
-    loop:true,
-    delay:75,
+    loop: true,
+    delay: 100,
 });
 
-
 typewriter
-.typeString('La capital del Sol')
+.typeString("La capital del Sol")
 .pauseFor(200)
 .start();
